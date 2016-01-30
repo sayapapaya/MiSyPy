d351d1ef2e6d2bc468feb4c69f3735db67b8f596
sayapapaya <sayadate@gmail.com>
2016-01-30 14:01:11 -0500

    can display html. basic index document created

--- pythonbee/pythonbee_app/views.py
+++ pythonbee/pythonbee_app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response, redirect
+from django.http import HttpResponse
+from django.template import Context, loader
 
 # Create your views here.
+
+def index(request):
+    #template = loader.get_template("index.html")
+     #C:\Python27\lib\site-packages\django\contrib\admin\templates\templates\index.html
+    #return HttpResponse("Hello, world. You're now at the pythonbee index.")
+    #return HttpResponse(template.render())
+    print ("\pythonbee_app\test")
+    #return render_to_response("\\pythonbee_app\\template\\index.html")
+    #return render_to_response("C:/Users/Saya/Documents/MiSyPy/MiSyPy/pythonbee/pythonbee_app/templates/index.html")
+    return render_to_response('index.html')
